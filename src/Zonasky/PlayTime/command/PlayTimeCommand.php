1cbed3a781eb699c74565a262d02cf24e3d96c9a
Inaayat <125380942+Inaay@users.noreply.github.com>
2023-04-11 05:37:09 -0700

    Update PlayTimeCommand.php

--- src/Zonasky/PlayTime/command/PlayTimeCommand.php
+++ src/Zonasky/PlayTime/command/PlayTimeCommand.php
@@ -11,14 +11,11 @@ use pocketmine\utils\TextFormat;
 use Zonasky\PlayTime\PlayTime;
 
 class PlayTimeCommand extends Command implements PluginOwned {
-
-	private $plugin;
     
-    public function __construct(PlayTime $plugin) {
+    public function __construct() {
         parent::__construct("playtime", "View your playtime");
 		$this->setAliases(["pt"]);
         $this->setPermission(DefaultPermissions::ROOT_USER);
-		$this->plugin = $plugin;
     }
     
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
@@ -36,12 +33,12 @@ class PlayTimeCommand extends Command implements PluginOwned {
             $sender->sendMessage(TextFormat::RED . "Could not find player " . $args[0]);
             return true;
         }
-        $playtime = PlayTime::getInstance()->getPlayTime($player);
+        $playtime = $this->getOwningPlugin()->getPlayTime($player);
         $sender->sendMessage(TextFormat::GREEN . $player->getName() . "'s Playtime: " . TextFormat::RED . $playtime);
         return true;
     }
 
 	public function getOwningPlugin() : PlayTime {
-		return $this->plugin;
+		return PlayTime::getInstance();
 	}
 }
